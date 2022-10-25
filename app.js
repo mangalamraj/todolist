848c4cd6d256f031a773d90281ead2219114f4f5
MANGALAM RAJ <mangalamraj@MANGALAMs-MacBook-Air.local>
2022-10-26 00:30:12 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+let items = ["Buy Food",
+"Cook Food",
+"Eat Food"];
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  let today = new Date();
+
+  let options = {
+      weekday: "long",
+      day: "numeric",
+      month: "long"
+  }
+
+  let day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", {kindOfday: day, newItem:items});
+});
+app.post("/", function(req,res){
+  let item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+})
+
+app.listen(3000, function() {
+  console.log("Server is up and running on port 3000");
+});
